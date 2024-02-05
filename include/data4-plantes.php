d686ffbd70bccffc11818644f883af2fb15988d0
Chloé JACOB <chloe.jacob4@etu.unistra.fr>
2024-02-05 11:54:31 +0100

    add "plantes" category

---
+++ include/data4-plantes.php
@@ -0,0 +1,76 @@
+<?php
+// Exemple des données pour les tests
+
+// Un tableau associatif
+$genepi = [
+    'name' => 'Génépi Jaune',
+    'description' => 'Le génépi jaune est une plante vivace aromatique de petite taille, 5 à 20 cm, au feuillage grisâtre et laineux, ce qui le protège du froid comme des U.V. d\'altitude. Ses fleurs jaunâtres sont minuscules et apparaissent pendant l\'été. Ce sont les hampes florales qui constituent les fameux « brins » de génépi.',
+    'photo' => 'https://images.squarespace-cdn.com/content/v1/612cfb796d4cc21b2b31ff6b/035de442-7b5f-4ccd-af7c-774e54452ec0/Photo+-+Plante+Actif+3.png',
+    'categories' => ['aromatique', 'alpine'],
+];
+
+$edelweiss = [
+    'name' => 'Edelweiss',
+    'description' => 'Symbole des montagnes alpines, l\'edelweiss est une plante aux fleurs blanches velues, souvent associée à la pureté.',
+    'photo' => 'https://img-3.journaldesfemmes.fr/vUFvLkeoxYGDFmF1ZBiPdlHCnoA=/1500x/smart/68ac12ed12f84f99bc0759fdc408dee2/ccmcms-jdf/10960187.jpg',
+    'categories' => ['emblématique', 'alpine'],
+];
+
+$arnica = [
+    'name' => 'Arnica des Montagnes',
+    'description' => 'Reconnue pour ses propriétés anti-inflammatoires, l\'arnica des montagnes est identifiable par ses grandes fleurs jaunes.',
+    'photo' => 'https://www.jardinsdefrance.org/wp-content/uploads/jdf-medias/images/JdF630/arnica-montana.jpg',
+    'categories' => ['médicinale', 'alpine'],
+];
+
+$gentiane = [
+    'name' => 'Gentiane Jaune',
+    'description' => 'La gentiane jaune est une plante vivace aux fleurs jaunes vives, utilisée à la fois pour les liqueurs apéritives et ses vertus médicinales.',
+    'photo' => 'https://chilowe.com/wp-content/uploads/2023/05/article-2023-9.jpg',
+    'categories' => ['aromatique', 'médicinale'],
+];
+
+$monkshood = [
+    'name' => 'Capuchon de Moine',
+    'description' => 'Le capuchon de moine, ou aconit, est une plante toxique aux fleurs bleu intense. Elle est souvent présente dans les régions alpines.',
+    'photo' => 'https://media.ooreka.fr/public/image/plant/1111/mainImage-full-12402768.jpg',
+    'categories' => ['toxique', 'alpine'],
+];
+
+$bluebell = [
+    'name' => 'Campanule',
+    'description' => 'La campanule, ou bluebell, est une plante à clochettes souvent bleues ou violettes. Elle égaye les prairies alpines de ses couleurs vives.',
+    'photo' => 'https://media.ooreka.fr/public/image/plant/8/mainImage-source-9131522.jpg',
+    'categories' => ['colorée', 'alpine'],
+];
+
+$mountain_pine = [
+    'name' => 'Pin à Crochets',
+    'description' => 'Le pin à crochets est un arbre adapté aux conditions alpines. Ses aiguilles courbées lui donnent un aspect caractéristique.',
+    'photo' => 'https://lestetardsarboricoles.fr/wordpress/wp-content/uploads/pin-a-crochets-achard09.jpg',
+    'categories' => ['arbre', 'alpine'],
+];
+
+$saxifrage = [
+    'name' => 'Saxifrage',
+    'description' => 'Les saxifrages sont des plantes résistantes qui colonisent les éboulis de montagne. Leur feuillage forme souvent des rosettes.',
+    'photo' => 'https://www.pepiniere-brochetlanvin.com/1483/Saxifraga-hostii-var-altissima-saxifrage-incrustee.jpg',
+    'categories' => ['résistante', 'alpine'],
+];
+
+$aster = [
+    'name' => 'Aster des Alpes',
+    'description' => 'L\'aster des Alpes est une plante à fleurs caractéristique des régions alpines. Ses pétales délicats illuminent les prairies.',
+    'photo' => 'https://www.plantes-shopping.fr/medias/cache//article_swiper_popup/articles/1625/01.jpg',
+    'categories' => ['floral', 'alpine'],
+];
+
+$heather = [
+    'name' => 'Bruyère',
+    'description' => 'La bruyère est une plante commune en montagne. Elle forme des coussinets denses et fleurit avec des petites fleurs roses ou violettes.',
+    'photo' => 'https://emova-monceaufleurs-fr-storage.omn.proximis.com/Imagestorage/images/2560/1600/5fb26ebf51a7e_bruyere_variete_callune.jpg',
+    'categories' => ['alpine'],
+];
+
+// ALL PLANTS
+$plantes_data = [$genepi, $edelweiss, $arnica, $gentiane, $monkshood, $bluebell, $mountain_pine, $saxifrage, $aster, $heather];
