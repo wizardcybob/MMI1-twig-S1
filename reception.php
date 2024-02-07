166be3650334164cabe0f99f2e1554da397c9f63
Chloé JACOB <chloe.jacob4@etu.unistra.fr>
2024-02-08 00:06:41 +0100

    form page contact

---
+++ reception.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<title>Réception de données de formulaires</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+	<!-- METHODE POST -->
+	<div>
+		<div>Variables reçues par la méthode POST :</div>
+		<?php
+			foreach ($_POST as $key => $value) {
+				print("<div>$key</div>");
+				print("<div>$value</div>");
+			}
+		?>
+	</div>
+	<!-- METHODE GET -->
+	<div>
+		<div>Variables reçues par la méthode GET :</div>
+		<?php
+			foreach ($_GET as $key => $value) {
+				print("<div>$key</div>");
+				print("<div>$value</div>");
+			}
+		?>
+	</div>
+</body>
+
+</html>
