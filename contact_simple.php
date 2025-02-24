b9368d3df073aa334fbb6db946c2333532fa6884
Chloe JACOB <chloejcb29@gmail.com>
2025-02-24 13:48:23 +0100

    exemple de formulaire simple

---
+++ contact_simple.php
@@ -0,0 +1,15 @@
+<?php
+    // EXEMPLE POUR UN FORMULAIRE PLUS SIMPLE
+    include('include/twig.php');
+    $twig = init_twig();
+
+
+    echo $twig->render('contact_simple.twig', [
+        'title_page' => 'Contact',
+        'titre' => 'Page contact',
+        'texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'data_formulaire_simple' => $_GET,
+    ]);
+
+
+?>
