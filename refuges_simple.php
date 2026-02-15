b14551aa3f2b494b177a1a82289f8fab64f3d9a1
Chloe JACOB <chloejcb29@gmail.com>
2026-02-15 22:34:24 +0100

    template simple

---
+++ refuges_simple.php
@@ -0,0 +1,19 @@
+<?php
+
+// Initialise Twig
+include('include/twig.php');
+$twig = init_twig();
+
+include('include/data1-refuges.php');
+
+// Lancement du moteur Twig :
+// $twig->render($modele-de-page, $tableau-de-variables)
+//
+// Le premier paramètre est le nom du modèle de page (le fichier Twig) à utiliser
+//
+// Le second paramètre est un tableau contenant les variables envoyées au modèle Twig
+// Chaque ligne indique 'nom-variable-twig' => valeur-variable-twig
+echo $twig->render('refuges_simple.twig', [
+	'title_page' => 'Refuges',
+	'all_refuges' => $refuges_data,
+]);
